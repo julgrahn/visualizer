dddab7817ebc21b828fdd24e4044aac02afd035e
julgrahn <alexander.grahn@hotmail.se>
2020-05-25 01:23:25 +0200

    added a grid and start + finish points

--- src/PathfindingVisualizer/PathfindingVisualizer.jsx
+++ src/PathfindingVisualizer/PathfindingVisualizer.jsx
@@ -1,20 +1,59 @@
-import React, {Component} from 'react';
-import Node from './Node/Node';
-import './PathfindingVisualizer.css';
+import React, { Component } from "react";
+import Node from "./Node/Node";
+
+import "./PathfindingVisualizer.css";
 
 export default class PathfindingVisualizer extends Component {
-    constructor (props) {
-        super(props);
-        this.state = {};
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      nodes: [],
+    };
+  }
 
-    render()
-    {
-        return (
-            <div>
-                Foo
-                <Node></Node>
-            </div>
-        )
+  componentDidMount() {
+    const nodes = [];
+    for (let row = 0; row < 15; row++) {
+      const currentRow = [];
+      for (let col = 0; col < 50; col++) {
+        const currentNode = {
+          col,
+          row,
+          isStart: row === 10 && col === 5,
+          isFinish: row === 10 && col === 45,
+        };
+        currentRow.push(currentNode);
+      }
+      nodes.push(currentRow);
     }
+    this.setState({ nodes });
+  }
+
+  render() {
+    const { nodes } = this.state;
+    console.log(nodes);
+
+    return (
+      <div className="grid">
+        {nodes.map((row, rowIdx) => {
+          return (
+            <div key={rowIdx}>
+              {row.map((node, nodeIdx) => {
+                const { isStart, isFinish } = node;
+                return (
+                  <Node
+                    key={nodeIdx}
+                    isStart={isStart}
+                    isFinish={isFinish}
+                    test={"foo"}
+                    //test={"kappa"}
+                  ></Node>
+                );
+              })}
+            </div>
+          );
+        })}
+      </div>
+    );
+  }
 }
